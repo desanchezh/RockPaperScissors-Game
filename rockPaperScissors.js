5260aa854753f536dab23f68dc952c86b9be3547
desanchezh <129230744+desanchezh@users.noreply.github.com>
2024-10-05 17:02:28 -0400

    Created input prompt and funciton to fix case

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,14 +1,27 @@
 let computerChoice = getComputerChoice ();
 function getComputerChoice () {
-    let randomNumber = Math.random() 
+    let randomNumber = Math.random(); 
     if (randomNumber <= (1/3)) {
         return "Rock";
     } else if (randomNumber <= (2/3)) {
         return "Paper";
     } else {
-        return "Scissors"
+        return "Scissors";
     }
 }
 
-console.log (computerChoice)
+let humanChoice = getHumanChoice ();
+function getHumanChoice () {
+    let response = prompt ("Type Rock, Paper, or Scissors", "no answer")
+    return String(response)
+}
+
+let humanChoiceStandard = toStandardizeCase ();
+function toStandardizeCase () {
+    let firstLetter = humanChoice.charAt(0).toUpperCase();
+    let restOfWord = humanChoice.slice(1).toLowerCase();
+    return firstLetter + restOfWord
+}
+
+console.log (humanChoiceStandard)
 
