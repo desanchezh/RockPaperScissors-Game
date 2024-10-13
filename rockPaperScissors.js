6b033d2ccac6c74c3737a5f4adbe62e8ce44b969
desanchezh <129230744+desanchezh@users.noreply.github.com>
2024-10-13 14:53:08 -0400

    cleanup and score display fix

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -14,19 +14,19 @@ let humanChoice = ""
 
 let rockButton = document.querySelector("#rockButton");
 rockButton.addEventListener("click", () => {
-    humanChoice = "Rock"
-    playRound()
-    //create a function that shows user selection and computer selection image//
+    humanChoice = "Rock";
+    playRound();
+    //create a function that shows user selection and computer selection image
 })
 let paperButton = document.querySelector("#paperButton");
 paperButton.addEventListener("click", () => {
-    humanChoice = "Paper"
-    playRound()
+    humanChoice = "Paper";
+    playRound();
 })
 let scissorsButton = document.querySelector("#scissorsButton");
 scissorsButton.addEventListener("click", () => {
-    humanChoice = "Scissors"
-    playRound()
+    humanChoice = "Scissors";
+    playRound();
 })
 
 let humanScore = 0;
@@ -48,43 +48,21 @@ result.appendChild(message);
 message.textContent = "make your choice."
 
 function playRound () {
-    computerChoice = getComputerChoice();
+    let computerChoice = getComputerChoice();
     if (computerChoice == humanChoice) {
-        message.textContent = "";
         message.textContent = `It's a tie! You both chose ${computerChoice}`;
-        currentScoreUser.textContent = "";
-        currentScoreComputer.textContent = "";
-        currentScoreUser.textContent = `Your score: ${humanScore}`;
-        currentScoreComputer.textContent = `Computer score: ${computerScore}`;
-        return "tie";
     } else if (humanChoice == "Rock" && computerChoice == "Scissors" || 
         humanChoice == "Scissors" && computerChoice == "Paper" ||
         humanChoice == "Paper" && computerChoice == "Rock") {
             humanScore++;
-            message.textContent = "";
             message.textContent = `You win! You chose ${humanChoice} and the computer chose ${computerChoice}`
-            currentScoreUser.textContent = "";
-            currentScoreComputer.textContent = "";
-            currentScoreUser.textContent = `Your score: ${humanScore}`;
-            currentScoreComputer.textContent = `Computer score: ${computerScore}`;
-            return "win";
         } else {
             computerScore++;
-            message.textContent = "";
             message.textContent = `You lose. You chose ${humanChoice} and the computer chose ${computerChoice}`;
-            currentScore.textContent = "";
-            currentScoreUser.textContent = "";
-            currentScoreComputer.textContent = "";
-            currentScoreUser.textContent = `Your score: ${humanScore}`;
-            currentScoreComputer.textContent = `Computer score: ${computerScore}`;
-            return "lose"}
+        }
+    currentScoreUser.textContent = `Your score: ${humanScore}`;
+    currentScoreComputer.textContent = `Computer score: ${computerScore}`;
     }
 
+
     //needs a hard cutoff after 5 rounds (reset button or reload page)//
-if (humanScore == 5) {
-    message.textContent = "";
-    message.textContent = "You win! :)";
-} else if (computerScore == 5) {
-    message.textContent = "";
-    message.textContent = "You lose :(";
-}
