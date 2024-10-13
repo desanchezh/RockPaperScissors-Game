4e589633c294a372f784e16a3a342a6e3d6fdc78
desanchezh <129230744+desanchezh@users.noreply.github.com>
2024-10-13 10:00:26 -0400

    added basic UI with DOM manipulation

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -10,47 +10,66 @@ function getComputerChoice () {
     }
 }
 
-//I realized too late (throwaway project) that I could have just checked the input by applying .toLowerCase to input and computer choice//
-function getHumanChoice () {
-    let response = prompt ("Rock, Paper, or Scissors", "type your choice")
-    function toStandardizeCase (response) {
-        let firstLetter = response.charAt(0).toUpperCase();
-        let restOfWord = response.slice(1).toLowerCase();
-    return firstLetter + restOfWord
-    }
-    return String(toStandardizeCase(response));
-}
+let humanChoice = ""
 
-function playGame () {
-    let humanScore = 0
-    let computerScore = 0
-    function playRound () {
-        computerChoice = getComputerChoice()
-        humanChoice =  getHumanChoice ()
-        if (computerChoice == humanChoice) {
-            console.log(`It's a tie! You both chose ${computerChoice}`);
-            return "tie";
+let rockButton = document.querySelector("#rockButton");
+rockButton.addEventListener("click", () => {
+    humanChoice = "Rock"
+    playRound()
+})
+let paperButton = document.querySelector("#paperButton");
+paperButton.addEventListener("click", () => {
+    humanChoice = "Paper"
+    playRound()
+})
+let scissorsButton = document.querySelector("#scissorsButton");
+scissorsButton.addEventListener("click", () => {
+    humanChoice = "Scissors"
+    playRound()
+})
 
-        } else if (humanChoice == "Rock" && computerChoice == "Scissors" || 
-            humanChoice == "Scissors" && computerChoice == "Paper" ||
-            humanChoice == "Paper" && computerChoice == "Rock") {
-                humanScore++;
-                console.log(`You win! You chose ${humanChoice} and the computer chose ${computerChoice}`);
-                return "win";
-                
+let humanScore = 0;
+let computerScore = 0;
+let score = document.querySelector("#score");
+let currentScore = document.createElement("span");
+score.appendChild(currentScore);
+
+let result = document.querySelector("#result");
+let message = document.createElement("span");
+result.appendChild(message);
+message.textContent = "make your choice."
+
+function playRound () {
+    computerChoice = getComputerChoice();
+    if (computerChoice == humanChoice) {
+        message.textContent = "";
+        message.textContent = `It's a tie! You both chose ${computerChoice}`;
+        currentScore.textContent = "";
+        currentScore.textContent = `Your score: ${humanScore} | Computer score: ${computerScore}`;
+        return "tie";
+    } else if (humanChoice == "Rock" && computerChoice == "Scissors" || 
+        humanChoice == "Scissors" && computerChoice == "Paper" ||
+        humanChoice == "Paper" && computerChoice == "Rock") {
+            humanScore++;
+            message.textContent = "";
+            message.textContent = `You win! You chose ${humanChoice} and the computer chose ${computerChoice}`
+            currentScore.textContent = "";
+            currentScore.textContent = `Your score: ${humanScore} | Computer score: ${computerScore}`;
+            return "win";
         } else {
             computerScore++;
-            console.log(`You lose. You chose ${humanChoice} and the computer chose ${computerChoice}`);
+            message.textContent = "";
+            message.textContent = `You lose. You chose ${humanChoice} and the computer chose ${computerChoice}`;
+            currentScore.textContent = "";
+            currentScore.textContent = `Your score: ${humanScore} | Computer score: ${computerScore}`;
             return "lose"}
     }
 
-    for (let i = 0; i < 5; i++) {
-        playRound ()
-    }
-
-    console.log("Player score: " + humanScore)
-    console.log("Computer score: " + computerScore)
-
-    }
-
-playGame()
+    //needs a hard cutoff after 5 rounds (reset button or reload page)//
+if (humanScore == 5) {
+    message.textContent = "";
+    message.textContent = "You win! :)";
+} else if (computerScore == 5) {
+    message.textContent = "";
+    message.textContent = "You lose :(";
+}
